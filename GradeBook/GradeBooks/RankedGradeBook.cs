b069e85aba4088ec11ed03c2ea3daf9c6a2fd1d2
Chris Hendrix <Chris.Hendrix@ClaytonHomes.com>
2018-07-16 10:44:50 -0400

    wip

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,10 +22,10 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException();
             }
 
-            if (averageGrade >= .8) returnValue = 'A';
-            if (averageGrade >= .6 && averageGrade < .8) returnValue = 'B';
-            if (averageGrade >= .4 && averageGrade < .6) returnValue = 'C';
-            if (averageGrade >= .2 && averageGrade < .4) returnValue = 'D';
+            if (averageGrade > .8) returnValue = 'A';
+            if (averageGrade > .6 && averageGrade <= .8) returnValue = 'B';
+            if (averageGrade > .4 && averageGrade <= .6) returnValue = 'C';
+            if (averageGrade > .2 && averageGrade <= .4) returnValue = 'D';
             return returnValue;
         }
     }
