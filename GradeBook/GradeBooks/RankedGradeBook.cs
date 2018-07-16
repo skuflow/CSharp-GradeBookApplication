50aad25a5db77fbf81c1dbed559bd12bd98dca25
Chris Hendrix <Chris.Hendrix@ClaytonHomes.com>
2018-07-16 10:42:28 -0400

    wip

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,22 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char returnValue = 'F';
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade >= .8) returnValue = 'A';
+            if (averageGrade >= .6 && averageGrade < .8) returnValue = 'B';
+            if (averageGrade >= .4 && averageGrade < .6) returnValue = 'C';
+            if (averageGrade >= .2 && averageGrade < .4) returnValue = 'D';
+            return returnValue;
         }
     }
 }
