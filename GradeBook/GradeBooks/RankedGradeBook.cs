59dc99a1d356917b0981184541e4e9e614ada8d7
Chris Hendrix <Chris.Hendrix@ClaytonHomes.com>
2018-07-16 11:27:03 -0400

    stat overrides

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -28,5 +28,29 @@ namespace GradeBook.GradeBooks
             if (averageGrade >= 20 && averageGrade < 40) returnValue = 'D';
             return returnValue;
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
